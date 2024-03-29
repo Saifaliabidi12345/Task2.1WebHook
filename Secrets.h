df5d685a27cda4c284b18f275d3a9ed279fe995d
Saifaliabidi12345 <saifaliabidi@gmail.com>
2024-03-29 23:28:58 +1000

    Secrets.h

---
+++ Secrets.h
@@ -0,0 +1,5 @@
+#define SECRET_SSID "Saif’s iPhone"		// replace MySSID with your WiFi network name
+#define SECRET_PASS "Ubitseca1.!"	// replace MyPassword with your WiFi password
+
+#define SECRET_CH_ID 2483158			// replace 0000000 with your channel number
+#define SECRET_WRITE_APIKEY "O0SPIMNV2WGXBZ9B"   // replace XYZ with your channel write API Key
